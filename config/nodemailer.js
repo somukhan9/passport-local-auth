9f71a20e36f2f769b8195fcbbb4d336dc328238d
Mehedi Hasan Samrat <mhsomrat154@gmail.com>
2021-05-22 22:49:18 +0600

    change in nodemailer conifg

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,7 +2,6 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  secure: true,
   auth: {
     user: process.env.NODE_MAILER_USER_EMAIL,
     pass: process.env.NODE_MAILER_USER_PASSWORD,
